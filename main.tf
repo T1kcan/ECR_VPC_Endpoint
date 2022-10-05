b869f851ea7f2759cddcdc058f1f99c42ed0f8bf
D1680_Tamer <tbincan@gmail.com>
2022-10-05 21:53:39 +0300

    policy

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_vpc_endpoint" "s3" {
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.private[0].id]
-  policy            = data.aws_iam_policy_document.s3_ecr_access.json
+  policy            = data.aws_iam_policy_document.s3_ecr_access #.json
 
 }
 
@@ -85,7 +85,7 @@ resource "aws_vpc_endpoint" "ecr-dkr-endpoint" {
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [aws_security_group.ecs_task.id]
   subnet_ids          = aws_subnet.private.*.id
-
+  
 }
 
 resource "aws_vpc_endpoint" "ecr-api-endpoint" {
